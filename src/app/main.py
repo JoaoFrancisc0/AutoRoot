07ee368fc366b500e7fd7f617677c333d6e00fd5
Joao Francisco <joaofrancisco1015@gmail.com>
2025-04-17 14:57:41 -0300

    inicio automação sga para a supervisao

--- src/app/main.py
+++ src/app/main.py
@@ -1,5 +1,5 @@
 from common import driver_manager, config_loader, google_drive
-from scripts import assistencia_veniti, assistencia_tw, evento_ileva, evento_kommo
+from scripts import assistencia_veniti, supervisao_sga
 
 
 def carregar_configuracoes(base_dir):
@@ -8,7 +8,9 @@ def carregar_configuracoes(base_dir):
         "assistencia_veniti_env": config_loader.load_env("assistencia_veniti", base_dir),
         "evento_ileva_env": config_loader.load_env("evento_ileva", base_dir),
         "evento_kommo_env": config_loader.load_env("evento_kommo", base_dir),
+        "supervisao_sga_env": config_loader.load_env("supervisao_sga", base_dir),
         "assistencia_veniti_selectors": config_loader.load_json("assistencia_veniti", base_dir),
+        "supervisao_sga_selectors": config_loader.load_json("supervisao_sga", base_dir)
     }
 
 
@@ -21,20 +23,12 @@ def autenticar_google_drive(base_dir):
     return google_drive.authenticate(base_dir)
 
 
-def automacao_assistencia_tw():
-    print("Automação TW ainda não implementada.")
-
-
 def automacao_assistencia_veniti(driver, urlLogin, selectors, values):
     assistencia_veniti.coleta_assistencia_veniti(driver, urlLogin, selectors, values)
 
 
-def automacao_evento_ileva():
-    print("Automação evento iLeva ainda não implementada.")
-
-
-def automacao_evento_kommo():
-    print("Automação evento Kommo ainda não implementada.")
+def automacao_supervisao_sga(driver, urlLogin, selectors, values):
+    supervisao_sga.coleta_supervisao_sga(driver, urlLogin, selectors, values)
 
 
 def main(base_dir):
@@ -43,24 +37,37 @@ def main(base_dir):
     
     configs = carregar_configuracoes(base_dir)
     
-    automacao_assistencia_tw()
-    automacao_assistencia_veniti(
+    # automacao_assistencia_veniti(
+    #     driver,
+    #     configs["assistencia_veniti_selectors"],
+    #     [
+    #         configs["assistencia_veniti_env"]["LOGIN_URL"],
+    #         configs["assistencia_veniti_env"]["HOME_URL"],
+    #         configs["assistencia_veniti_env"]["ATENDIMENTO_URL"],
+    #         configs["assistencia_veniti_env"]["CONJUNTURA_URL"],
+    #     ],
+    #     [
+    #         configs["assistencia_veniti_env"]["USERNAME"],
+    #         configs["assistencia_veniti_env"]["PASSWORD"],
+    #         configs["assistencia_veniti_env"]["ID_EXTRA"]
+    #     ]
+    # )
+
+    automacao_supervisao_sga(
         driver,
-        configs["assistencia_veniti_selectors"],
+        configs["supervisao_sga_selectors"],
         [
-            configs["assistencia_veniti_env"]["LOGIN_URL"],
-            configs["assistencia_veniti_env"]["HOME_URL"],
-            configs["assistencia_veniti_env"]["ATENDIMENTO_URL"],
-            configs["assistencia_veniti_env"]["CONJUNTURA_URL"],
+            configs["supervisao_sga_env"]["LOGIN_URL"],
+            configs["supervisao_sga_env"]["HOME_URL"],
+            configs["supervisao_sga_env"]["VEICULO_URL"],
+            configs["supervisao_sga_env"]["BOLETO_URL"],
         ],
         [
-            configs["assistencia_veniti_env"]["USERNAME"],
-            configs["assistencia_veniti_env"]["PASSWORD"],
-            configs["assistencia_veniti_env"]["ID_EXTRA"]
+            configs["supervisao_sga_env"]["USERNAME"],
+            configs["supervisao_sga_env"]["PASSWORD"]
         ]
     )
-    automacao_evento_ileva()
-    automacao_evento_kommo()
+
     print("Automação finalizada.")
     input("Pressione Enter para sair...")
 
---
+++ src/scripts/supervisao_sga.py
@@ -0,0 +1,3 @@
+def coleta_supervisao_sga(driver, urlLogin, selectors, values):
+    print(driver, urlLogin, selectors, values)
+    pass
