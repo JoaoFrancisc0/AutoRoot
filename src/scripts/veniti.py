0aa4d8fb60352e54bc77539dc5d34854f59b5aec
Joao Francisco <joaofrancisco1015@gmail.com>
2025-04-24 17:09:19 -0300

    refatoração do selectors veniti

--- src/scripts/veniti.py
+++ src/scripts/veniti.py
@@ -14,6 +14,8 @@ def coleta_atendimentos(service, driver, selectors, url, folder_id, tipo):
     ui_actions.detectar_e_clicar_n_elementos(driver, selectors["busca"])
     ui_actions.preencher_periodo_mensal_atual(driver, selectors["periodo"])
     ui_actions.detectar_e_clicar_n_elementos(driver, selectors["atributos"])
+    # aguardar o valor de ["download"]["status"] ser = Exportado
+    # clicar no botão ["download"]["download"]
 
 
 def coleta_veniti(service, driver, selectors, configs):
@@ -21,7 +23,10 @@ def coleta_veniti(service, driver, selectors, configs):
     if (scheduler.verificacao_data_veniti(dia, dia_semana, hora)):
         url = configs["url"]
         values = configs["credenciais"]
-        folder_id = configs["folder_id"]    
+        folder_id = configs["folder_id"]
+
         login_veniti(driver, url["login_url"], url["home_url"], selectors, values)
+
+        selectors = selectors["relatorio"]
         if (scheduler.agendamento_coleta_atendimentos(dia, dia_semana, hora)):
-            coleta_atendimentos(service, driver, selectors, url["atendimento_url"], folder_id, tipo="atendimentos")
+            coleta_atendimentos(service, driver, selectors["atendimentos"], url["atendimento_url"], folder_id, tipo="atendimentos")
