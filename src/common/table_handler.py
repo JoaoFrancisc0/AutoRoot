446346c33ca6f4835706fa70fb26a9c28346bc18
Joao Francisco <joaofrancisco1015@gmail.com>
2025-06-27 16:49:24 -0300

    Add remove_lines_outside_month_filter()

--- src/common/table_handler.py
+++ src/common/table_handler.py
@@ -1,4 +1,4 @@
-from src.common import BeautifulSoup
+from src.common import BeautifulSoup, pd
 
 def add_quotes_to_columns(file_path, column_indices):
     try:
@@ -38,4 +38,23 @@ def add_quotes_to_columns(file_path, column_indices):
         return new_file_path
     except Exception as e:
         print(f"Error processing the file: {e}")
+        raise
+
+
+def remove_lines_outside_month_filter(file_path, column_index, month):
+    try:
+        print(f"Lendo o arquivo '{file_path}'...")
+        df = pd.read_excel(file_path)
+        column_name = df.columns[column_index]
+        df_filtered = df[~df[column_name].astype(str).str.contains(month, case=False, na=False)]
+        # .astype(str) para garantir que a comparação funcione mesmo se a coluna tiver números, datas, etc.
+        # .str.contains() para verificar a ocorrência da string (ignorando maiúsculas/minúsculas opcionalmente)
+        df_filtered.to_excel(file_path, index=False) # index=False evita salvar o índice do DataFrame como uma coluna no Excel
+
+    except FileNotFoundError:
+        print(f"Error file '{file_path}' not found")
+        raise
+    
+    except Exception as e:
+        print(f"Error: {e}")
         raise
